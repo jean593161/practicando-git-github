b7ca860599285116a3125bcb4c66a1cfb810fa46
JeanPool Lopez Vara <135078066+jean593161@users.noreply.github.com>
2025-07-10 09:03:23 -0400

    subiendo archivo de python

---
+++ codigo.py
@@ -0,0 +1,7 @@
+nombre = "jean"
+apellido = "lopez"
+
+saludo = f"Hola {nombre} {apellido}, bienvenido al curso de Python."
+
+print(saludo)
+print(f"Hola {nombre} {apellido}, bienvenido al curso de Python. que te valla bien en el curso.")
